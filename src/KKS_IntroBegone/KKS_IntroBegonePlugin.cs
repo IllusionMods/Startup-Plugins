f40d8cd5638ba86e209b0944a95e947dc567422b
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2021-10-23 22:41:18 +0200

    Added KKS_IntroBegone

--- src/KKS_IntroBegone/KKS_IntroBegonePlugin.cs
+++ src/KKS_IntroBegone/KKS_IntroBegonePlugin.cs
@@ -5,17 +5,16 @@ using Scene = Manager.Scene;
 
 namespace IntroBegone
 {
-    //[BepInProcess("Koikatu")]
-    //[BepInProcess("Koikatsu Party")]
-    //[BepInDependency(KKAPI.KoikatuAPI.GUID, KKAPI.KoikatuAPI.VersionConst)]
+    [BepInProcess(KKAPI.KoikatuAPI.GameProcessName)]
+    [BepInDependency(KKAPI.KoikatuAPI.GUID, KKAPI.KoikatuAPI.VersionConst)]
     public partial class IntroBegonePlugin : BaseUnityPlugin
     {
         private void Awake()
         {
-            //var skip = Config.Bind("Game startup", "Skip showing intro screen", true,
-            //    "Speed up game startup by skippping the logo shown at the start, before title screen.");
-            //
-            //if (skip.Value)
+            var skip = Config.Bind("Game startup", "Skip showing intro screen", true,
+                "Speed up game startup by skippping the logo shown at the start, before title screen.");
+
+            if (skip.Value)
                 Harmony.CreateAndPatchAll(typeof(HooksSkip), GUID);
         }
 
@@ -25,34 +24,19 @@ namespace IntroBegone
             [HarmonyPatch(typeof(LogoScene), "Start")]
             private static bool DisableLogo(ref IEnumerator __result)
             {
-                //IEnumerator LoadTitle()
-                //{
-                //    Logger.LogInfo("Skipping intro");
-                //    Singleton<Scene>.Instance.LoadReserve(new Scene.Data
-                //    {
-                //        levelName = "Title",
-                //        isFade = false
-                //    }, false);
-                //    yield break;
-                //}
-
-                
-                Scene.LoadReserveAsyncForget(new Scene.Data
+                IEnumerator LoadTitle()
                 {
-                    levelName = "CustomScene",
-                    isAdd = false,
-                    fadeType = FadeCanvas.Fade.In,
-                    onLoad = delegate
+                    Logger.LogInfo("Skipping intro");
+                    Scene.LoadReserve(new Scene.Data
                     {
-                        var rootComponent = Scene.GetRootComponent<CustomScene>("CustomScene");
-                        if (rootComponent == null) return;
-                        rootComponent.modeNew = true;
-                        rootComponent.modeSex = 1;
-                        rootComponent.chaFileCtrl = null;
-                    }
-                }, true);
+                        levelName = "Title",
+                        isFade = false,
+                        fadeType = FadeCanvas.Fade.None
+                    }, false);
+                    yield break;
+                }
 
-                //__result = LoadTitle();
+                __result = LoadTitle();
                 return false;
             }
         }
